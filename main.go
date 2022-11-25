0ce4021f634c8321a376b84c074283a5689d0cc8
insidepacket <david_zhangw@hotmail.com>
2022-11-25 21:43:23 +1100

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Sorry! You are hacked"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
